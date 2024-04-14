54e09ca6a1e9f5fe933aaca6be10c804dc2378c0
DevilForDevs <ranjanpanpura@gmail.com>
2024-04-14 20:28:00 +0530

    searchbar using edit text

---
+++ app/src/main/java/com/ranjan/searchbar/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ranjan.searchbar
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*val searchView: SearchView = findViewById(R.id.search_view)
+        searchView.setIconifiedByDefault(false)*/
+
+    }
+}
---
+++ app/src/main/java/com/ranjan/searchbar/SearchableActivity.kt
@@ -0,0 +1,14 @@
+package com.ranjan.searchbar
+
+import android.app.SearchManager
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchable)
+
+    }
+}
