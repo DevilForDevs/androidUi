0204d1a64bc1e4f8e724ad523039e9b596cb9982
DevilForDevs <ranjanpanpura@gmail.com>
2024-04-15 06:50:57 +0530

    searchbar in actionbar

--- app/src/main/java/com/ranjan/searchbar/MainActivity.kt
+++ app/src/main/java/com/ranjan/searchbar/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.ranjan.searchbar
+
+
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 
@@ -8,8 +11,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*val searchView: SearchView = findViewById(R.id.search_view)
-        searchView.setIconifiedByDefault(false)*/
 
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_searchbar, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView?
+        searchView?.isIconified = false
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return true
+            }
+        })
+        return true
+    }
+
+
 }
